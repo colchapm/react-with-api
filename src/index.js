7edd4bc24b70363549a9e6a4e9c978f7dbfa8627
Collin Chapman <cchap14@gmail.com>
2021-09-19 11:43:26 -0700

    add middleware library and custom middleware to the store

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/headlines-reducer';
+import { Provider } from 'react-redux';
+import { ThunkMiddleware } from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
+//first argument is our reducer, the second argument is the applyMiddleware function with the arguments we want to apply to the store - this is how we make our redux store ready to use middleware
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
